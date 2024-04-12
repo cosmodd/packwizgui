30a1875418f5e6c9d189a23b5267f5466926cd11
cosmodd <lolesqua@gmail.com>
2024-04-12 10:56:51 +0200

    🚸 Project carsd are now clickable

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -8,23 +8,23 @@
 	let hovered = false;
 </script>
 
-<div
+<a
+	href="/projects/{project.id}"
 	class="flex flex-row bg-white rounded-lg border border-gray-200 {padding} hover:bg-gray-100 transition-all"
 	on:mouseenter={() => (hovered = true)}
 	on:mouseleave={() => (hovered = false)}
-	role="listitem"
 >
 	<h1 class="text-xl font-bold grow whitespace-nowrap truncate">{project.name}</h1>
 	<div
 		class="buttons flex items-center gap-1 transition-all"
 		class:opacity-0={!hovered}
 	>
-		<a
+		<!-- <a
 			href="/projects/{project.id}"
 			class="text-white bg-blue-500 hover:bg-blue-600 p-1 rounded transition-all"
 		>
 			<Icon src={FolderOpen} micro class="w-4 h-4" />
-		</a>
+		</a> -->
 		<button
 			class="text-white bg-red-500 hover:bg-red-600 p-1 rounded transition-all"
 			on:click={() => projects.delete(project.id)}
@@ -32,4 +32,4 @@
 			<Icon src={Trash} micro class="w-4 h-4" />
 		</button>
 	</div>
-</div>
+</a>
