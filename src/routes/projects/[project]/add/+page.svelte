473b4dd10392d20fd57d336119fd2b377b51d78a
cosmodd <lolesqua@gmail.com>
2024-04-21 20:51:35 +0200

    🍱 Placeholder page for adding content to packs

---
+++ src/routes/projects/[project]/add/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+
+	const projectType = $page.url.searchParams.get("type") ?? "mod" as "mod" | "resourcepack" | "shader";
+</script>
+
+<h1 class="text-3xl font-bold">Add {projectType}</h1>
